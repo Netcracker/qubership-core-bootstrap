591eb939392d0efdfc97125c6a581eaf5902c0b0
alsergs <sergei.s.aleksandrov.qubership@gmail.com>
2025-07-28 09:41:25 +0300

    chore: script adaptation to local/aws installation

---
+++ cloud-core-local-dev/aws.mk
@@ -0,0 +1,56 @@
+# Configuration file for cloud-core-local-dev
+# This file contains configurable values that can be overridden
+
+# Installation configuration
+# parameters used by script, but not propagated to helm values
+CREATE_NAMESPACE ?= false
+INSTALL_CRDS ?= false
+INSTALL_METRICS_SERVER ?= false
+INSTALL_MONITORING ?= false
+INSTALL_CONSUL ?= false
+INSTALL_DBAAS ?= true
+DBAAS_CONFIG_FILE ?= aws.mk
+
+# Namespace configuration
+CORE_NAMESPACE ?= core-1-core
+CONSUL_NAMESPACE ?= consul
+MONITORING_NAMESPACE ?= monitoring
+# pg & dbaas namespaces are propagated to dbaas-install
+PG_NAMESPACE ?= core-1-postgres
+DBAAS_NAMESPACE ?= core-1-dbaas
+
+# General values
+DEPLOYMENT_SESSION_ID ?= cloud-core-aws-dev
+MONITORING_ENABLED ?= false
+CONSUL_SERVICE_NAME ?= consul-server
+CONSUL_ENABLED ?= true
+
+# DBaaS configuration
+# propagated to dbaas-install
+DBAAS_SERVICE_NAME ?= dbaas-aggregator
+
+# Core bootstrap configuration
+CORE_BOOTSTRAP_IMAGE ?= ghcr.io/netcracker/core-bootstrap:latest 
+CORE_CONFIG_CONSUL_ENABLED ?= false
+
+# Components values
+FACADE_OPERATOR_TAG ?= latest
+
+INGRESS_GATEWAY_TAG ?= main-snapshot
+INGRESS_GATEWAY_CLOUD_PUBLIC_HOST ?= svc.cluster.local
+INGRESS_GATEWAY_CLOUD_PRIVATE_HOST ?= svc.cluster.local
+
+CONTROL_PLANE_TAG ?= latest
+
+PAAS_MEDIATION_TAG ?= latest
+
+DBAAS_AGENT_TAG ?= latest
+
+CORE_OPERATOR_TAG ?= feature-docker-image-build-snapshot
+CORE_OPERATOR_IMAGE_REPOSITORY ?= ghcr.io/netcracker/qubership-core-core-operator
+
+CONFIG_SERVER_TAG ?= feature-docker-image-build-snapshot
+CONFIG_SERVER_IMAGE_REPOSITORY ?= ghcr.io/netcracker/qubership-core-config-server
+CONFIG_SERVER_CONSUL_ENABLED ?= false
+
+SITE_MANAGEMENT_TAG ?= feature-local-deployment-snapshot
