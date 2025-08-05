cdc1de2d066f6ad798251cc1c3e66872c17dffa2
Evgenii Stetsenko <i@estetsenko.com>
2025-08-05 17:21:55 +0300

    fix: pass context

--- cr-synchronizer/main.go
+++ cr-synchronizer/main.go
@@ -13,7 +13,7 @@ func main() {
 	var isPostDeployPhase bool
 	flag.BoolVar(&isPostDeployPhase, "post", false, "use cr-synchronizer as post-deploy waiter")
 	flag.Parse()
-	timeoutSeconds := 30
+	timeoutSeconds := 300 // 5min
 	if val, ok := os.LookupEnv("RESOURCE_POLLING_TIMEOUT"); ok {
 		if parsed, err := strconv.Atoi(val); err == nil {
 			timeoutSeconds = parsed
