058632f37902d90ad164e2c54ff3a8e8cba54808
Evgenii Stetsenko <i@estetsenko.com>
2025-08-07 16:10:13 +0300

    fix: react on os signals

--- cr-synchronizer/main.go
+++ cr-synchronizer/main.go
@@ -3,21 +3,31 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/netcracker/cr-synchronizer/getters"
 	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
 )
 
 func main() {
-	ctx := context.Background()
-	var isPostDeployPhase bool
-	flag.BoolVar(&isPostDeployPhase, "post", false, "use cr-synchronizer as post-deploy waiter")
-	flag.Parse()
-	timeoutSeconds := 300 // 5min
-	if val, ok := os.LookupEnv("RESOURCE_POLLING_TIMEOUT"); ok {
-		if parsed, err := strconv.Atoi(val); err == nil {
-			timeoutSeconds = parsed
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
+
+	go func() {
+		var isPostDeployPhase bool
+		flag.BoolVar(&isPostDeployPhase, "post", false, "use cr-synchronizer as post-deploy waiter")
+		flag.Parse()
+		timeoutSeconds := 300 // 5min
+		if val, ok := os.LookupEnv("RESOURCE_POLLING_TIMEOUT"); ok {
+			if parsed, err := strconv.Atoi(val); err == nil {
+				timeoutSeconds = parsed
+			}
 		}
-	}
-	getters.StartGenerator(ctx, isPostDeployPhase, timeoutSeconds)
+		getters.StartGenerator(ctx, isPostDeployPhase, timeoutSeconds)
+	}()
+
+	<-ctx.Done()
+	fmt.Println("received shutdown signal, exiting...")
 }
