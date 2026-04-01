cfc7d530f78ab5dceb8f5866c6015e4f1e55bbba
alsergs <sergei.s.aleksandrov.qubership@gmail.com>
2026-04-01 15:36:09 +0300

    feat: refine istio repo url

--- cloud-core-local-dev/istio/aws.mk
+++ cloud-core-local-dev/istio/aws.mk
@@ -4,16 +4,16 @@
 # Namespace settings
 CREATE_NAMESPACE ?= false
 ISTIO_NAMESPACE ?= istio-system
+SKIP_CRDS ?= true
 
 #===============================================
 # Gateway API CRDs
-SKIP_CRDS ?= true
 GATEWAY_API_VERSION ?= v1.4.0
 GATEWAY_API_CHANNEL ?= standard
 
 #===============================================
 # Istio chart repository settings
-ISTIO_REPO_URL ?= https://github.com/Netcracker/qubership-istio-distr.git
+ISTIO_REPO_URL ?= https://github.com/Netcracker/qubership-istio.git
 ISTIO_REPO_BRANCH ?= main
 ISTIO_RELEASE_NAME = qubership-istio
 # Extra helm args (optional), e.g.: ISTIO_HELM_EXTRA_ARGS = --set someKey=someValue
