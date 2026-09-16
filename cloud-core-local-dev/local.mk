3c33c278fe60142e66cb0997bbae1d94a3501134
Karina Jexembiyeva <karinajexembiyeva@gmail.com>
2026-09-16 19:10:16 +0500

    feat: update docker image tag

--- cloud-core-local-dev/local.mk
+++ cloud-core-local-dev/local.mk
@@ -53,7 +53,7 @@ KAFKA_INSTANCES ?= kafka-1
 RABBIT_INSTANCES ?= 
 
 # Core bootstrap configuration
-CORE_BOOTSTRAP_IMAGE ?= ghcr.io/netcracker/core-bootstrap:latest 
+CORE_BOOTSTRAP_IMAGE ?= ghcr.io/netcracker/core-bootstrap:migrate-on-Declarative-DbaaS-e6a1469
 CORE_CONFIG_CONSUL_ENABLED ?= false
 CORE_CONFIG_MAAS_ENABLED ?= false
 CORE_CONFIG_MAAS_INTERNAL_ADDRESS ?= http://maas-service.maas:8080
